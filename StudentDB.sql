575c0f85fe6a2a97ea8c058b35dc2e346e1dc922
SergeiySt <ser.st.1989@gmail.com>
2023-05-20 19:15:47 +0300

    add_5

---
+++ StudentDB.sql
@@ -0,0 +1,20 @@
+﻿create database StudentDB;
+
+
+use StudentDB
+go
+
+
+create table Student 
+(
+	id_student int not null identity(1,1) primary key,
+	SSurName nvarchar(50) check (SSurName <> '') not null,
+	SName nvarchar(50) check (SName  <> '') not null,
+	SAge int not null
+);
+go
+
+insert into Student values
+('Кириленко', 'Жора', 22),
+('Сидоренко', 'Світлана', 25);
+go
